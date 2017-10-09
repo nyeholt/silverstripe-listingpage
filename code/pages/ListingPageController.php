89ff502b6e538db144c2500b32dbfa3cfeda44a4
Jake B <jake@symbiote.com.au>
2017-10-09 17:39:23 +1100

    fix(ListingPageController): Fix security issue related to actions

--- code/pages/ListingPageController.php
+++ code/pages/ListingPageController.php
@@ -2,21 +2,22 @@
 
 namespace Symbiote\ListingPage;
 
+
 use PageController;
+use SilverStripe\Control\HTTPRequest;
 
 class ListingPageController extends PageController {
     private static $url_handlers = array(
         '$Action' => 'index'
     );
 
-    public function index() {
-
+    public function index(HTTPRequest $request) {
         // This is required so the listing page doesn't eat AJAX requests against the page controller.
-        $action = $this->getRequest()->latestParam('Action');
-        if ($action && $this->hasMethod($action)) {
+        $action = $request->latestParam('Action');
+        if ($action && $this->hasMethod($action) &&
+            in_array($action, $this->config()->allowed_actions)) {
             return $this->$action();
-        }
-        else if (($this->data()->ContentType || $this->data()->CustomContentType)) {
+        } else if (($this->data()->ContentType || $this->data()->CustomContentType)) {
             // k, not doing it in the theme...
             $contentType = $this->data()->ContentType ? $this->data()->ContentType : $this->data()->CustomContentType;
             $this->response->addHeader('Content-type', $contentType);
